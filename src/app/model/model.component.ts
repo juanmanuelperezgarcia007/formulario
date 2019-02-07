637025c228406babd1e09eb38fb57af7b44b7124
jymmyneutron <jymmyneutron007@gmail.com>
2019-02-07 12:53:08 +0100

    validaciones echas

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -13,11 +13,18 @@ export class ModelComponent implements OnInit {
 
   ngOnInit() {
       this.formRegistro = new FormGroup({
-      nombre: new FormControl('', [Validators.required]),
-      email: new FormControl('',[Validators.required]),
+      
+        nombre: new FormControl('', [Validators.required, Validators.maxLength(15)]),
+      
+      email: new FormControl('',[Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),Validators.required]),
+      
+      password: new FormControl('',[Validators.required, Validators.pattern(/(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/)]),
+      
       telefono: new FormControl('',[Validators.required]),
-      fechaNacimiento: new FormControl('',[Validators.required]),
-      dni: new FormControl('',[Validators.required])
+      
+      fechaNacimiento: new FormControl('',[this.fechaNacimientoValidator, Validators.required]),
+      
+      dni: new FormControl('',[Validators.required,Validators.pattern(/(0?[1-9]|[1-9][0-9])[0-9]{6}(-| )?[trwagmyfpdxbnjzsqvhlcke]/)])
 
     })
   }
@@ -26,4 +33,14 @@ export class ModelComponent implements OnInit {
     this.formRegistro.reset()//limpiar campos despues del envio.
   }
 
+  fechaNacimientoValidator(control){
+    if(isNaN(control.value)){
+      return {'fechanacimiento': 'Debe ser un numero'}
+    }
+    if(control.value < 1980 || control.value > 2010){
+      return {'fechanacimiento': 'El año debe estar entre 1980 y 2010'}
+    }
+     return null
+  }
+  
 }
