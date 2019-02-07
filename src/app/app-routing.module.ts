db5ab17656eccaba16039f02aff2a06173ec170e
jymmyneutron <jymmyneutron007@gmail.com>
2019-02-07 09:48:35 +0100

    creacion de html form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateComponent } from './template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: '/tempalte',pathMatch:'full'},
+  { path: 'template', component:TemplateComponent },
+  {path: "**", redirectTo: '/template'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
